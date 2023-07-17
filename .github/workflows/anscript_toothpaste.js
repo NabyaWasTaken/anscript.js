f04c16046c186432ea5166c089c707335d21df66
nabyawastaken <139764901+nabyawastaken@users.noreply.github.com>
2023-07-17 16:52:43 +0530

    Update anscript_toothpaste.js

--- .github/workflows/anscript_toothpaste.js
+++ .github/workflows/anscript_toothpaste.js
@@ -1,5 +1,5 @@
 const slides = [
-  "https://cdn.shopify.com/s/files/1/0565/7814/6463/products/PHOTO-2021-03-03-02-11-47_grande.jpg?v=1655380134",
+  "https://cdn.shopify.com/s/files/1/0565/7814/6463/products/PHOTO-2021-03-03-02-11-47_grande.jpg",
   "https://static-bebeautiful-in.unileverservices.com/natural_remedies_for_acne_treatment_at_home_mobilehome.jpg",
   "https://aliyakg.flpretail.com/media/cache/product/photo/6b/6ba4/6ba44be4/6ba44be4fdb942dc/bdc5d8a57c61148cb370ff31450cd5f2.png"
 ];
